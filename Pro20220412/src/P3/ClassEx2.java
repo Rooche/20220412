f4e10b14395d8e547fbc0f4ffb094369b4ab9994
admin <admin@YD2-21>
2022-04-13 10:07:06 +0900

    과제

--- Pro20220412/src/P3/ClassEx2.java
+++ Pro20220412/src/P3/ClassEx2.java
@@ -5,19 +5,45 @@ import java.util.Scanner;
 public class ClassEx2 {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		int ran = (int)(Math.random() * 3) + 1;
 		boolean isTrue = true;
-		int a, b, c;
-				
-		while(isTrue) {
+
+		while (isTrue) {
 			System.out.println("1.가위 2.바위 3.보");
+			int ran = (int) (Math.random() * 3) + 1;
+			System.out.println("숫자를 선택하세요:");
 			int user = scn.nextInt();
-			if (user == ran) {
-				System.out.println("비겼다");
-				break;
+
+			if (user == 1) {
+				if (ran == 2) {
+					System.out.println("You lose");
+				} else if (ran == 3) {
+					System.out.println("You win");
+				} else if (ran == 1) {
+					System.out.println("Drawn");
+				} else {
+					if (user == 2) {
+						if (ran == 2) {
+							System.out.println("Drawn");
+						} else if (ran == 3) {
+							System.out.println("You lose");
+						} else if (ran == 1) {
+							System.out.println("You win");
+						}
+					} else {
+						if (user == 3) {
+							if (ran == 3) {
+								System.out.println("Drawn");
+							} else if (ran == 1) {
+								System.out.println("You lose");
+							} else {
+								System.out.println("You win");
+							} 
+						
+						}
+					}
+				}
 			}
 		}
-		
 	}
 
 }
